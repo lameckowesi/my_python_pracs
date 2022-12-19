9d4da1c6adc64ceac0d7ded972ccab6f27fd1d93
Lameck <lameckowesi@gmail.com>
2022-12-19 21:16:22 +0000

    created new files to explore lists

---
+++ nested_lists.py
@@ -0,0 +1,7 @@
+contractors = ["Martin", "Josephat", "David", "Ogum"]
+architects = ["vincent", "Eric", "Brenda", "Shem"]
+qs = ["Herbert", "Njau", "Lorine"]
+
+professionals = [contractors, architects, qs]
+
+print (professionals)
