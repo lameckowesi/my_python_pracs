e2db7f321bd0bf0cd1e8766d342b6d77347d76ad
Lameck <lameckowesi@gmail.com>
2022-12-19 22:09:56 +0000

    added winning and losing logic

--- rock_paper_scissors.py
+++ rock_paper_scissors.py
@@ -1,3 +1,5 @@
+import random
+
 rock = '''
     _______
 ---'   ____)
@@ -24,6 +26,34 @@ scissors = '''
       (____)
 ---.__(___)
 '''
-
+# store the options in a list
 options = [rock, paper, scissors]
-print(options[1])
+
+# take input from the user 
+your_input = int(input("Please choose 0 for rock, 1 for paper or 2 for scissors: "))
+
+# assign user an option based on the input entered
+your_choice = options[your_input]
+
+# computer random option
+computer_choice = random.choice(options)
+
+# show user what they chose vs what the computer chose
+print (f"You chose {your_choice} and the computer chose {computer_choice}") 
+
+# check if user entered wrong number
+if your_input >=3:
+    print("You entered an invalid number")
+
+# logic to tell if you win or lose 
+elif your_choice == rock and computer_choice == scissors:
+    print("You win")
+elif your_choice == paper and computer_choice ==rock:
+    print("You win")
+elif your_choice ==scissors and computer_choice == paper:
+    print("You win")
+elif your_choice == computer_choice:
+    print ("It is a draw")
+else:
+    print ("You lose")
+    
